2bebe5fcbe65a0f9f4554d92ded8c512533716ec
adhunchanchoc <adhunchanchoc@gmail.com>
2023-01-18 14:04:08 +0100

    Updates

---
+++ precache-manifest.95647251b66c310bf3b2c30f3fe2c189.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0249f42becdc0a957d00",
+    "url": "/react-diggers-app/static/css/main.9aeca34d.chunk.css"
+  },
+  {
+    "revision": "0249f42becdc0a957d00",
+    "url": "/react-diggers-app/static/js/main.0249f42b.chunk.js"
+  },
+  {
+    "revision": "7e2bd9f02f6bc8a0930a",
+    "url": "/react-diggers-app/static/js/1.7e2bd9f0.chunk.js"
+  },
+  {
+    "revision": "a6c022e65890005b4c42",
+    "url": "/react-diggers-app/static/js/runtime~main.a6c022e6.js"
+  },
+  {
+    "revision": "f3e0839b11225cb379c05a39ce2b79cd",
+    "url": "/react-diggers-app/index.html"
+  }
+];
